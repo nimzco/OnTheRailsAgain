fd2889c3faa6c06255d190d5e10fd6e76a614d88
Nima Izadi <nim.izadi@gmail.com>
2011-07-26 21:28:09 +0200

    Removing ajax

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,10 @@
 class ArticlesController < InheritedResources::Base
   before_filter :authenticate_author!, :only => ["new", "create"]
   
+  def new
+    @article = Article.new
+    @article.authors << current_author
+    new!
+  end
+    
 end
