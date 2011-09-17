254d632de7eed5898dda761fe85a8a00c7d1ea44
Nima Izadi <nim.izadi@gmail.com>
2011-09-18 00:31:17 +0200

    Removing share buttons from footer and adding it before comments

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 OntheRailsAgain::Application.initialize!
-Encoding.default_internal, Encoding.default_external = ['utf-8'] * 2
