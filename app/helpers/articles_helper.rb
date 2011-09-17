f0ae56aeae44acc79e1cf3495ec33295caaeb2a2
Nima Izadi <nim.izadi@gmail.com>
2011-09-17 23:20:38 +0200

    Deleted migration relative to comment table

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,15 +1,5 @@
 module ArticlesHelper
 
-  def nested_comments(comments)
-    comments.map do |comment, sub_comment|
-      if sub_comment.empty?
-        render(:partial => "comments/comment", :locals => {:comment => comment})
-      else
-        render(:partial => "comments/comment", :locals => {:comment => comment}) + nested_comments(sub_comment)
-      end
-    end.join.html_safe
-  end  
-
   def join_tags(article)
     article.tags.map { |tag| link_to tag.name, articles_path(:tag => tag.name) }.join(" - ").html_safe
   end
