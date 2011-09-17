747d8ada71f2b2c671a2dd53fd7d2419994c8352
Nima Izadi <nim.izadi@gmail.com>
2011-09-17 23:49:37 +0200

    Adding encoding UTF-8 to environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 OntheRailsAgain::Application.initialize!
+Encoding.default_internal, Encoding.default_external = ['utf-8'] * 2
