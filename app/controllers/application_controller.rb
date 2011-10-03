b75c8ef1401dfadba7cdea5ce8583724d07275d6
Nima Izadi <nim.izadi@gmail.com>
2011-10-03 11:31:17 +0200

    Favicon changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
-  #  
-  # def record_not_found
-  #   render :file => "#{Rails.root}/public/404.html", :status => :not_found, :layout => false
-  # end
-
 end
