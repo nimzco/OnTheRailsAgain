4a944af897fc3eff24b3aa123e8b746f82e9d28d
Nima Izadi <nim.izadi@gmail.com>
2011-10-03 10:37:02 +0200

    Removing CSS class in error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
- 
-  def record_not_found
-    render :file => "#{Rails.root}/public/404.html", :status => :not_found
-  end
+  # rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+  #  
+  # def record_not_found
+  #   render :file => "#{Rails.root}/public/404.html", :status => :not_found, :layout => false
+  # end
 
 end
