ba1a27b4422debcd766dc0da96aa25003d36e275
Nima Izadi <nim.izadi@gmail.com>
2011-10-03 10:21:14 +0200

    404 Redirection corrected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
  
-  private
- 
   def record_not_found
-    render "shared/error404", :status => 404
+    render :file => "#{Rails.root}/public/404.html", :status => :not_found
   end
 
 end
