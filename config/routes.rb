c77dfe10e917998f2248e479e489fec7dfc34f98
Nima Izadi <nim.izadi@gmail.com>
2011-10-10 12:11:29 +0200

    Add rss auto discovery in header

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ OntheRailsAgain::Application.routes.draw do
   root :to => "articles#index"  
   
   match "sitemap.xml", :to => "sitemap#index", :defaults => {:format => :xml}
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
