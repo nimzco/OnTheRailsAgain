0f805957df81c95a6c5d4a668ef05a91b12437a0
Nima Izadi <nim.izadi@gmail.com>
2011-10-11 09:02:04 +0200

    Bugfix for redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_filter :check_url
 
   def check_url
-    redirect_to request.protocol + request.host_with_port + request.request_uri, :status => 301 if /^www/.match(request.host)
+    redirect_to request.protocol + request.host_with_port[4..-1] + request.fullpath, :status => 301 if /^www/.match(request.host)
   end
 end
