24b48ca41070008a83979cfbacd7223ca942d8b5
Nima Izadi <nim.izadi@gmail.com>
2012-04-20 21:36:13 +0200

    Add CoffeeTouch.js for the tap on mobile. Continued cutting in files for the css layout.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,24 +1,28 @@
 //= require jquery
 //= require twitter/bootstrap
+//= require libs/CoffeeTouch
 //= require libs/jquery.scrollTo-1.4.2-min
 //= require libs/jquery.localscroll-1.2.7-min
+
 $(function() {
 	// Popover
   var search_button = $('#search_button');
   var tag_button  	 = $('#tag_button');
   search_button.popover({placement: 'left', trigger: 'manual'});
   tag_button   .popover({placement: 'left', trigger: 'manual'});
-  $('body').click(function(jQueryEvent) {
-  	var id = jQueryEvent.srcElement.id;
+  
+
+  $('body').onGesture('tap', function(CoffeeTouchEvent) {
+  	var id = CoffeeTouchEvent.targets[0].id;
   	if (id === 'tag_button') {
   		search_button.popover('hide');
   		tag_button.popover('toggle');
   	} else if (id === 'search_button') {
   		tag_button.popover('hide');
   		search_button.popover('toggle');
-  	} else if ($(jQueryEvent.srcElement).closest('.popover').length === 0) {
+  	} else if ($(CoffeeTouchEvent.targets[0]).closest('.popover').length === 0) {
 		  search_button.popover('hide');
 			tag_button   .popover('hide');
   	};
-  });
+  });  
 });
