ed826fbfb7547fc7dc5d5b9c6456ebc60c350ab2
Nima Izadi <nim.izadi@gmail.com>
2012-11-02 17:53:09 +0100

    Extension fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ OntheRailsAgain::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile  += ['libs/prettify.js', 'libs/prettify.css', 'article_show.js', 'nima.less'] # %w( search.js )
+  config.assets.precompile  += ['libs/prettify.js', 'libs/prettify.css', 'article_show.js', 'nima.css'] # %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
